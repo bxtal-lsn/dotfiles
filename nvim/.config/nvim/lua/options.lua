873e6b4b799243fd5ff3efbd04b1dc6a71da3713
Bartal Læarsson <bl@sev.fo>
2025-05-13 15:06:49 +0100

    refine readme

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -5,6 +5,42 @@ vim.opt.relativenumber = true
 
 vim.opt.showmode = false
 
+local function is_wsl()
+  local output = vim.fn.system 'grep -i microsoft /proc/version'
+  return (output ~= '' and output:find 'microsoft' ~= nil)
+end
+
+-- Set clipboard based on environment
+if is_wsl() then
+  -- WSL clipboard configuration using win32yank
+  vim.g.clipboard = {
+    name = 'win32yank',
+    copy = {
+      ['+'] = 'win32yank -i --crlf',
+      ['*'] = 'win32yank -i --crlf',
+    },
+    paste = {
+      ['+'] = 'win32yank -o --lf',
+      ['*'] = 'win32yank -o --lf',
+    },
+    cache_enabled = 0,
+  }
+else
+  -- Linux clipboard configuration using xclip
+  vim.g.clipboard = {
+    name = 'xclip',
+    copy = {
+      ['+'] = 'xclip -selection clipboard',
+      ['*'] = 'xclip -selection clipboard',
+    },
+    paste = {
+      ['+'] = 'xclip -selection clipboard -o',
+      ['*'] = 'xclip -selection clipboard -o',
+    },
+    cache_enabled = 1,
+  }
+end
+
 vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
 end)
